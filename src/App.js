ab4b1fa30224c575cbb48690d3e49f3babb3506b
Precious <precious.d.bey@gmail.com>
2022-06-12 17:17:27 -0400

    restarting pup project in react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Precious Peagle Pups</h1>
+    </div>
+  );
+}
+
+export default App;
