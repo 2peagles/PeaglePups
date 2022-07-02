cb260293aee805d2502819c2732658247d788f4d
preciousdbey <preciousdbey@penguin.lxd>
2022-07-02 02:05:45 -0400

    got particles just need to customizes it, then just need to get my form to sumbit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './About.css';
+import Particles from 'react-tsparticles';
+import { loadFull } from "tsparticles";
 // import pillow1 from '../../Image/pillow1.jpg';
 // import outside from '../../Image/outside.jpg';
 // import ss1 from '../../Image/ss1.jpg';
@@ -10,8 +12,97 @@ import Col from 'react-bootstrap/Col'
 
 
 export const About = ()=>{
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return(
   <Container fluid id='about'>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          background: {
+            color: {
+              value: "transparent",
+            },
+          },
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
     <Row className='about-container'>
       <Col md lg xl xxl>
         <Col className='about-content' >
